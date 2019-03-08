47bbda02753df2121019eac07c5be380fbc7b232
Cala Torra <000404667@bga.upb>
2019-03-08 13:10:34 -0500

    ya casi tengo numero primo

--- primos.py
+++ primos.py
@@ -1,8 +1,8 @@
 a=int(input("ingrese numero a"))
 b=int(input("ingrese numero b"))
 
-if a%b==0:
-    print("el numero a es divisible exactamente entre b")
-else:
-    print("el numero a no es divisible exactamente entre b")
-    
+for i in range (1,a):
+    i=i+1
+    res=i%a
+    print(res)
+
